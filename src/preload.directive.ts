b4fe8f4bd5542f168f64aa25d5af3d24fea38c02
flauc <filip.lauc93@gmail.com>
2017-08-11 21:34:05 +0200

    feat: Initial setup :tada:

---
+++ src/preload.directive.ts
@@ -0,0 +1,19 @@
+import {Directive, ElementRef, Input, Renderer2} from '@angular/core';
+import {PreloadService} from '../services/preload.service';
+
+@Directive({
+  selector: '[jaspero-preload]'
+})
+export class PreloadDirective {
+  constructor(
+    private _el: ElementRef,
+    private _renderer: Renderer2,
+    private _service: PreloadService
+  ) {}
+
+  @Input() loadBg = false;
+  @Input('jaspero-preload') set preload(img: string) {
+    this._renderer.setAttribute(this._el.nativeElement, this.loadBg ? 'data-bg' : 'data-src', img);
+    this._service.addImage(this._el.nativeElement);
+  }
+}
